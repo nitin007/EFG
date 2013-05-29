c09bbbc8e15e4028cd80462b9b97671cfada8206
James Abley <james.abley@gmail.com>
2013-05-29 08:17:07 +0100

    Use updated_at instead
    created_at doesn't appear to be consistently populated in the
    user_audits table. Will raise a separate story to look at that.
    
    Also appear to have an issue with the IL0 data extract, which drops the
    created_at field and sets it to the current time.

--- db/migrate/20130527182739_set_password_changed_at.rb
+++ db/migrate/20130527182739_set_password_changed_at.rb
@@ -20,9 +20,9 @@ class SetPasswordChangedAt < ActiveRecord::Migration
      SuperUser,
      SystemUser].each do |user_class|
       user_class.where(:disabled => false).each do |user|
-        password_changed = user.user_audits.where(:function => UserAudit::PASSWORD_CHANGED).order("created_at DESC").first
-        user.password_changed_at = if password_changed
-          password_changed.created_at
+        password_changed = user.user_audits.where(:function => UserAudit::PASSWORD_CHANGED).order("updated_at DESC").first
+        user.password_changed_at = if password_changed && password_changed.updated_at
+          password_changed.updated_at
         else
           Time.new(2013, 3, 7, 0, 0, 0, 0)
         end
