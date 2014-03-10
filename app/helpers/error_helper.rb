ba415680b056e1eac4ae700b033e5891ce270c00
Niall Mullally <niall.mullally@gmail.com>
2014-03-10 17:42:27 +0000

    Loan repayment duration change validation errors weren't showing

--- app/helpers/error_helper.rb
+++ app/helpers/error_helper.rb
@@ -8,4 +8,22 @@ module ErrorHelper
     end
   end
 
+  def render_attribute_errors(form, *attribute_names)
+    html = [:amount, :repayment_duration].collect do |attribute|
+      if form.object.errors.include?(attribute)
+        content_tag :div, class: "control-group error" do
+          content_tag :div, class: "controls" do
+            content_tag :div, class: "help-inline" do
+              form.object.errors[attribute].join(', ')
+            end
+          end
+        end
+      end
+    end
+
+    return if html.empty?
+
+    html.join('')
+  end
+
 end
