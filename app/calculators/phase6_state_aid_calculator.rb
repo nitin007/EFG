7aee31e66095043ac01e8978b8eaea8f48a14220
Ben Pickles <spideryoung@gmail.com>
2014-04-30 12:27:38 +0100

    Slightly reduce complexity.

--- app/calculators/phase6_state_aid_calculator.rb
+++ app/calculators/phase6_state_aid_calculator.rb
@@ -1,7 +1,8 @@
 class Phase6StateAidCalculator
   ConservativeEuroConversionRate = 1.25
-
   MaximumStateAidAmount = Money.new(200_000_00, 'EUR')
+  MaxLoanAmount = Money.new(1_200_000_00)
+  MaxRepaymentDuration = 60.0
 
   attr_reader :loan
 
@@ -11,8 +12,8 @@ class Phase6StateAidCalculator
 
   def state_aid_eur
     MaximumStateAidAmount *
-      (amount / max_loan_amount) *
-        (repayment_duration / max_repayment_duration) *
+      (amount / MaxLoanAmount) *
+        (repayment_duration / MaxRepaymentDuration) *
           (euro_conversion_rate / ConservativeEuroConversionRate)
   end
 
@@ -21,20 +22,11 @@ class Phase6StateAidCalculator
       loan.amount || Money.new(0)
     end
 
-    def max_loan_amount
-      Money.new(1_200_000_00)
-    end
-
     def repayment_duration
       loan.repayment_duration.try(:total_months) || 0
-    end
-
-    def max_repayment_duration
-      60.0
     end
 
     def euro_conversion_rate
       loan.euro_conversion_rate
     end
-
 end
