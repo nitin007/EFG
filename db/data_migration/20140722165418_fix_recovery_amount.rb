8116d8e27b5c19f50a485cda0162e1b96d2f2842
Olly Legg <olly@new-bamboo.co.uk>
2014-07-22 17:54:39 +0100

    Alter amount due on recovery for Loan: 9VAEU63+01.
    Requested by Liam Clayton on 2014-07-18.

---
+++ db/data_migration/20140722165418_fix_recovery_amount.rb
@@ -0,0 +1,15 @@
+# This is intended to provide a mechanism by which relatively expensive data
+# updates can be decoupled from deployment time and run independently.
+#
+# This will execute within an ActiveRecord transaction and is instance_eval-ed.
+# Just write normal ruby code and refer to any model objects that you require.
+# Since a data migration is intended to be run in production, when the schema
+# and model are known quantities, it should be fine to reference model classes
+# directly, even though in the future they may be refactored or deleted
+# entirely.
+
+recovery = Recovery.find_by_id(4820)
+if recovery
+  recovery.amount_due_to_dti = Money.new(596_43)
+  recovery.save!
+end
