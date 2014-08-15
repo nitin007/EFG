f9ebbbe4ccc5caf819d66c93da87a9783bd85e0e
James Abley <james.abley@gmail.com>
2014-08-15 13:45:24 +0100

    Don’t install development gems in CI
    debugger doesn’t work under 2.1.2 and isn’t needed.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
+bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment --without=development
 RAILS_ENV=test bundle exec rake db:setup
 RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test COVERAGE=1 bundle exec rake ci:setup:rspec parallel:prepare parallel:spec
