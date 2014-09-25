a2d019f6e57c8e48571a1555dd766c7559bbdf82
Ben Pickles <spideryoung@gmail.com>
2014-09-25 12:25:30 +0100

    Protect an already fully-drawn loan from having its draws reprofiled.

--- app/presenters/reprofile_draws_loan_change.rb
+++ app/presenters/reprofile_draws_loan_change.rb
@@ -1,8 +1,15 @@
 class ReprofileDrawsLoanChange < LoanChangePresenter
+  LoanAlreadyFullyDrawnError = Class.new(StandardError)
+
   include CapitalRepyamentHolidayFields
   include TrancheDrawdownsFields
 
   before_save :update_loan_change
+
+  def initialize(loan, _)
+    raise LoanAlreadyFullyDrawnError unless loan.can_reprofile_draws?
+    super
+  end
 
   private
     def update_loan_change
