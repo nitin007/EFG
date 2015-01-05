08ba368b683e7c529774a81c78f0d77243f5b6fe
Ben Pickles <spideryoung@gmail.com>
2015-01-05 11:13:32 +0000

    We'll switch to :json at some point in the future.

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.action_dispatch.cookies_serializer = :marshal
+# TODO: Switch to `:json` at some point in the future.
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
