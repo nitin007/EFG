9b7ca285e2558ac58c1c6f05d24fe1c097785c6c
Niall Mullally <niall.mullally@gmail.com>
2015-01-21 16:34:33 +0000

    Capital repayment holiday loan changes can only be entered when loan is fully drawn.
    When the loan is not fully drawn capital repayment holidays must be 
    entered on the Reprofile Draws page.
    This is to ensure that the generated premium schedule is correct.

--- spec/requests/loan_changes/capital_repayment_holiday_loan_change_request_spec.rb
+++ spec/requests/loan_changes/capital_repayment_holiday_loan_change_request_spec.rb
@@ -6,35 +6,52 @@ describe 'Capital repayment loan change' do
   let(:loan) { FactoryGirl.create(:loan, :guaranteed, amount: Money.new(100_000_00), maturity_date: Date.new(2014, 12, 25), repayment_duration: 60, repayment_frequency_id: RepaymentFrequency::Quarterly.id) }
 
   before do
-    loan.initial_draw_change.update_column(:date_of_change, Date.new(2009, 12, 25))
-
     visit loan_path(loan)
-    click_link 'Change Amount or Terms'
-    click_link 'Capital Repayment Holiday'
   end
 
-  it do
-    fill_in :date_of_change, '11/9/10'
-    fill_in :initial_draw_amount, '65,432.10'
-    fill_in :initial_capital_repayment_holiday, '3'
-
-    Timecop.freeze(2010, 9, 1) do
-      click_button 'Submit'
+  context 'when the loan has NOT drawn its full amount' do
+    before do
+      loan.initial_draw_change.update_column(:amount_drawn, Money.new(50_000_00).cents)
     end
 
-    loan_change = loan.loan_changes.last!
-    loan_change.change_type.should == ChangeType::CapitalRepaymentHoliday
-    loan_change.date_of_change.should == Date.new(2010, 9, 11)
+    it do
+      click_link 'Change Amount or Terms'
+      expect(page).to_not have_content('Capital Repayment Holiday')
+    end
+  end
 
-    premium_schedule = loan.premium_schedules.last!
-    premium_schedule.initial_draw_amount.should == Money.new(65_432_10)
-    premium_schedule.premium_cheque_month.should == '12/2010'
-    premium_schedule.repayment_duration.should == 48
-    premium_schedule.initial_capital_repayment_holiday.should == 3
+  context 'when the loan has drawn its full amount' do
+    before do
+      loan.initial_draw_change.update_column(:amount_drawn, loan.amount.cents)
+      loan.initial_draw_change.update_column(:date_of_change, Date.new(2009, 12, 25))
 
-    loan.reload
-    loan.modified_by.should == current_user
-    loan.repayment_duration.total_months.should == 60
-    loan.maturity_date.should == Date.new(2014, 12, 25)
+      click_link 'Change Amount or Terms'
+      click_link 'Capital Repayment Holiday'
+    end
+
+    it do
+      fill_in :date_of_change, '11/9/10'
+      fill_in :initial_draw_amount, '65,432.10'
+      fill_in :initial_capital_repayment_holiday, '3'
+
+      Timecop.freeze(2010, 9, 1) do
+        click_button 'Submit'
+      end
+
+      loan_change = loan.loan_changes.last!
+      loan_change.change_type.should == ChangeType::CapitalRepaymentHoliday
+      loan_change.date_of_change.should == Date.new(2010, 9, 11)
+
+      premium_schedule = loan.premium_schedules.last!
+      premium_schedule.initial_draw_amount.should == Money.new(65_432_10)
+      premium_schedule.premium_cheque_month.should == '12/2010'
+      premium_schedule.repayment_duration.should == 48
+      premium_schedule.initial_capital_repayment_holiday.should == 3
+
+      loan.reload
+      loan.modified_by.should == current_user
+      loan.repayment_duration.total_months.should == 60
+      loan.maturity_date.should == Date.new(2014, 12, 25)
+    end
   end
 end
