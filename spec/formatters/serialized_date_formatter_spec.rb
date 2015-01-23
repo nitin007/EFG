d0c0930e83cf9b48be228554b39f97e0a0c714ab
Ben Pickles <spideryoung@gmail.com>
2015-01-23 14:53:22 +0000

    I like this way of structuring specs.

--- spec/formatters/serialized_date_formatter_spec.rb
+++ spec/formatters/serialized_date_formatter_spec.rb
@@ -2,22 +2,30 @@ require 'spec_helper'
 
 describe SerializedDateFormatter do
   describe '::format' do
-    it 'correctly formats dd/mm/yyyy and returns a Date object' do
-      SerializedDateFormatter.format('11/1/2011').should == Date.new(2011, 1, 11)
+    subject { SerializedDateFormatter.format(input) }
+
+    context 'with input dd/mm/yyyy' do
+      let(:input) { '11/1/2011' }
+      it { should eql(Date.new(2011, 1, 11)) }
     end
 
-    it 'correctly formats dd/mm/yy and returns a Date object' do
-      SerializedDateFormatter.format('11/1/11').should == Date.new(2011, 1, 11)
+    context 'with input dd/mm/yy' do
+      let(:input) { '11/1/11' }
+      it { should eql(Date.new(2011, 1, 11)) }
     end
   end
 
   describe '::parse' do
-    it 'correctly parses dd/mm/yyyy and returns a String' do
-      SerializedDateFormatter.parse('11/1/2011').should == '2011-01-11'
+    subject { SerializedDateFormatter.parse(input) }
+
+    context 'with input dd/mm/yyyy' do
+      let(:input) { '11/1/2011' }
+      it { should eql('2011-01-11') }
     end
 
-    it 'correctly parses dd/mm/yy and returns a String' do
-      SerializedDateFormatter.parse('11/1/11').should == '2011-01-11'
+    context 'with input dd/mm/yy' do
+      let(:input) { '11/1/11' }
+      it { should eql('2011-01-11') }
     end
   end
 end
