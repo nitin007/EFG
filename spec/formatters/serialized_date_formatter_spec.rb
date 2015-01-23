772c3bcb97be5ad3f7a7b4f19329192fb672919a
Ben Pickles <spideryoung@gmail.com>
2015-01-23 14:49:31 +0000

    Fix specs.

--- spec/formatters/serialized_date_formatter_spec.rb
+++ spec/formatters/serialized_date_formatter_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe SerializedDateFormatter do
   describe '::format' do
-    it 'correctly parses dd/mm/yyyy and returns a Date object' do
+    it 'correctly formats dd/mm/yyyy and returns a Date object' do
       SerializedDateFormatter.format('11/1/2011').should == Date.new(2011, 1, 11)
     end
 
-    it 'correctly parses dd/mm/yy and returns a Date object' do
+    it 'correctly formats dd/mm/yy and returns a Date object' do
       SerializedDateFormatter.format('11/1/11').should == Date.new(2011, 1, 11)
     end
   end
 
   describe '::parse' do
-    it 'correctly parses dd/mm/yyyy and returns a Date object' do
-      SerializedDateFormatter.format('11/1/2011').should == Date.new(2011, 1, 11)
+    it 'correctly parses dd/mm/yyyy and returns a String' do
+      SerializedDateFormatter.parse('11/1/2011').should == '2011-01-11'
     end
 
-    it 'correctly parses dd/mm/yy and returns a Date object' do
-      SerializedDateFormatter.format('11/1/11').should == Date.new(2011, 1, 11)
+    it 'correctly parses dd/mm/yy and returns a String' do
+      SerializedDateFormatter.parse('11/1/11').should == '2011-01-11'
     end
   end
 end
