6933cfd19fa2691f5430a83d33a9b0e49aaab378
Neil Atkinson <neil@new-bamboo.co.uk>
2015-01-23 17:32:00 +0000

    Explain what to do when no sub-lender options in data correction

--- app/presenters/data_corrections/sub_lender_data_correction.rb
+++ app/presenters/data_corrections/sub_lender_data_correction.rb
@@ -9,6 +9,12 @@ class SubLenderDataCorrection < DataCorrectionPresenter
 
   validate :sub_lender_is_blank, unless: :lender_has_sub_lenders?
 
+  def no_sub_lenders_hint
+    unless lender_has_sub_lenders?
+      "There are no sub-lenders for this loan's lender. If you wish to remove the existing sub-lender from this loan, submit this form with a blank selection."
+    end
+  end
+
   private
 
   def lender_has_sub_lenders?
