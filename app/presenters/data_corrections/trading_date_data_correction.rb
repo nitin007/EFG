7d053187d48a8a1e90fa85507b994bcfe5baa528
Ben Pickles <spideryoung@gmail.com>
2015-01-23 15:31:22 +0000

    Ensure trading_date is stored as a Date when set from user input.

--- app/presenters/data_corrections/trading_date_data_correction.rb
+++ app/presenters/data_corrections/trading_date_data_correction.rb
@@ -2,4 +2,8 @@ class TradingDateDataCorrection < DataCorrectionPresenter
   include BasicDataCorrectable
 
   data_corrects :trading_date
+
+  def trading_date=(value)
+    @trading_date = QuickDateFormatter.parse(value)
+  end
 end
