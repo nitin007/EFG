63af6e6715a335c4659bcf6d732c1c6913494456
Neil Atkinson <neil@new-bamboo.co.uk>
2015-01-23 15:52:37 +0000

    Sub-lender data correction must be set if a lender has sub-lenders

--- app/presenters/data_corrections/sub_lender_data_correction.rb
+++ app/presenters/data_corrections/sub_lender_data_correction.rb
@@ -1,7 +1,16 @@
 class SubLenderDataCorrection < DataCorrectionPresenter
   include BasicDataCorrectable
 
-  data_corrects :sub_lender
+  data_corrects :sub_lender, skip_validation: true
 
   delegate :sub_lender_names, to: :loan
+
+  validates_inclusion_of :sub_lender, in: :sub_lender_names, if: :lender_has_sub_lenders?
+
+  private
+
+  def lender_has_sub_lenders?
+    sub_lender_names.present?
+  end
+
 end
--- spec/presenters/data_corrections/sub_lender_data_correction_spec.rb
+++ spec/presenters/data_corrections/sub_lender_data_correction_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe SubLenderDataCorrection do
-  it_behaves_like 'a basic data correction presenter', :sub_lender, 'Bar'
+  let(:current_user) { FactoryGirl.create(:lender_user) }
+  let(:data_correction) { FactoryGirl.build(:sub_lender_data_correction, loan: loan, created_by: current_user) }
+
+  context "when lender has sub-lenders" do
+    let!(:loan) { FactoryGirl.create(:loan, :guaranteed, lender: current_user.lender) }
+    let!(:sub_lender) { FactoryGirl.create(:sub_lender, lender: loan.lender, name: 'ACME Sub-lender') }
+
+    it_behaves_like 'loan updating data correction presenter', :sub_lender, 'ACME Sub-lender'
+
+    it "must have a sub-lender" do
+      data_correction.sub_lender = nil
+      data_correction.should_not be_valid
+      data_correction.should have(1).error_on(:sub_lender)
+    end
+
+    it "must have an allowed sub-lender" do
+      data_correction.sub_lender = "not a valid sub lender for this lender"
+      data_correction.should_not be_valid
+      data_correction.should have(1).error_on(:sub_lender)
+    end
+  end
 end
