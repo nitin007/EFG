a5332000e76b41b992f2ed0dfd023701609d21ef
Niall Mullally <niall.mullally@gmail.com>
2015-02-06 17:20:58 +0000

    Bootstrapify loan sub category custom input

--- app/inputs/loan_sub_category_select_input.rb
+++ app/inputs/loan_sub_category_select_input.rb
@@ -4,7 +4,11 @@ class LoanSubCategorySelectInput < SimpleForm::Inputs::Base
       raise ArgumentError, 'Please specify the :loan_category_id'
     end
 
-    @builder.collection_select(attribute_name, select_options, :id, :name, prompt: 'Please select', input_html: { class: 'input-xxlarge' })
+    input_options[:prompt] = 'Please select'
+
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+    @builder.collection_select(attribute_name, select_options, :id, :name, input_options, merged_input_options)
   end
 
   private
