41cca81e45076f1a7f0b102d5d7fdbb498f03388
Niall Mullally <niall.mullally@gmail.com>
2015-02-06 11:29:22 +0000

    Bootstrapify DED code custom input

--- app/inputs/ded_code_input.rb
+++ app/inputs/ded_code_input.rb
@@ -1,5 +1,7 @@
 class DedCodeInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
-    @builder.select(attribute_name, options.delete(:select_options), input_options, input_html_options)
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+    @builder.select(attribute_name, options.delete(:select_options), input_options, merged_input_options)
   end
 end
