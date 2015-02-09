4bbef150085987571384312aa7e5712cd57cc88e
Niall Mullally <niall.mullally@gmail.com>
2015-02-09 13:05:59 +0000

    Different bootstrap-sass javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap-sprockets
 //= require 'jquery-ui.autocomplete'
 //= require 'jquery.placeholder.min'
 //= require 'jquery.peity.min'
