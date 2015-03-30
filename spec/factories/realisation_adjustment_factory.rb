f52d0243ae4a3e726e1956ceae3ab539c44d247e
Ben Pickles <spideryoung@gmail.com>
2015-03-30 12:00:41 +0100

    Can use a FactoryGirl association.

--- spec/factories/realisation_adjustment_factory.rb
+++ spec/factories/realisation_adjustment_factory.rb
@@ -3,9 +3,6 @@ FactoryGirl.define do
     created_by factory: :cfe_user
     amount Money.new(100_00)
     date { Date.current }
-
-    after(:build) do |realisation_adjustment|
-      realisation_adjustment.loan ||= FactoryGirl.create(:loan, :realised)
-    end
+    loan factory: [:loan, :realised]
   end
 end
