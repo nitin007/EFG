2a0626d1a97dd2e71b05d77a5ea8f286dcc84eb9
Ben Pickles <spideryoung@gmail.com>
2015-04-08 18:20:40 +0100

    Pass dates around as "quickformat".

--- spec/requests/reports/realisation_report_spec.rb
+++ spec/requests/reports/realisation_report_spec.rb
@@ -53,8 +53,8 @@ describe 'Realisation report' do
   let(:lender4) { loan4.lender }
   let(:lender5) { loan5.lender }
 
-  let(:report_start_date) { 4.days.ago.to_date.to_s }
-  let(:report_end_date) { 2.day.ago.to_date.to_s }
+  let(:report_start_date) { 4.days.ago.to_date.to_s(:screen) }
+  let(:report_end_date) { 2.day.ago.to_date.to_s(:screen) }
 
   before(:each) do
     login_as(current_user, scope: :user)
@@ -65,9 +65,6 @@ describe 'Realisation report' do
 
     context 'when selecting particular lenders' do
       it 'outputs a CSV realisation report' do
-        report_start_date = 4.days.ago.to_date.to_s
-        report_end_date = 2.day.ago.to_date.to_s
-
         visit root_path
         click_link 'Generate Realisations Report'
 
@@ -96,8 +93,6 @@ describe 'Realisation report' do
 
     context 'when selecting ALL lenders' do
       it 'outputs a CSV realisation report for all lenders' do
-        report_start_date = 4.days.ago.to_date.to_s
-        report_end_date = 2.day.ago.to_date.to_s
         all_lenders_list = Lender.all.map(&:name).join(', ')
 
         visit root_path
@@ -123,10 +118,10 @@ describe 'Realisation report' do
     end
 
     context 'when report criteria returns no results' do
-      it 'a csv file cannot be downloaded' do
-        report_start_date = 10.days.ago.to_date.to_s
-        report_end_date = 9.days.ago.to_date.to_s
+      let(:report_start_date) { 10.days.ago.to_date.to_s(:screen) }
+      let(:report_end_date) { 9.days.ago.to_date.to_s(:screen) }
 
+      it 'a csv file cannot be downloaded' do
         visit root_path
         click_link 'Generate Realisations Report'
 
@@ -153,51 +148,57 @@ describe 'Realisation report' do
 
   end
 
-  context 'as an lender user' do
-    let!(:current_user) { FactoryGirl.create(:lender_user, lender: lender1) }
+  context 'as a lender user' do
+    let(:current_user) { FactoryGirl.create(:lender_user, lender: lender1) }
 
-    it "outputs a CSV realisation report for the current user's lender" do
-      visit root_path
-      click_link 'Generate Realisations Report'
+    context 'with results' do
+      let(:report_start_date) { 4.days.ago.to_date.to_s(:screen) }
+      let(:report_end_date) { 1.days.ago.to_date.to_s(:screen) }
 
-      expect(page).to have_text 'Realisations Report Criteria'
-      fill_in 'What is the start date for your report?', with: 4.days.ago.to_date.to_s
-      fill_in 'What is the end date for your report?', with: 1.day.ago.to_date.to_s
-      expect(page).not_to have_text('Which lenders would you like to see realisations for?')
+      it "outputs a CSV realisation report for the current user's lender" do
+        visit root_path
+        click_link 'Generate Realisations Report'
 
-      click_button 'Submit'
+        expect(page).to have_text 'Realisations Report Criteria'
+        fill_in 'What is the start date for your report?', with: report_start_date
+        fill_in 'What is the end date for your report?', with: report_end_date
+        expect(page).not_to have_text('Which lenders would you like to see realisations for?')
 
-      page.should have_text 'Data extract found 2 rows'
-      expect(page).not_to have_text 'Lenders'
-      click_button 'Download Report'
+        click_button 'Submit'
 
-      page.response_headers['Content-Type'].should include('text/csv')
+        page.should have_text 'Data extract found 2 rows'
+        expect(page).not_to have_text 'Lenders'
+        click_button 'Download Report'
+
+        page.response_headers['Content-Type'].should include('text/csv')
+      end
     end
 
-    it 'a csv file cannot be downloaded when report criteria returns no results' do
-      report_start_date = 10.days.ago.to_date.to_s
-      report_end_date = 9.days.ago.to_date.to_s
+    context 'with no results' do
+      let(:report_start_date) { 10.days.ago.to_date.to_s(:screen) }
+      let(:report_end_date) { 9.days.ago.to_date.to_s(:screen) }
 
-      visit root_path
-      click_link 'Generate Realisations Report'
+      it 'a csv file cannot be downloaded when report criteria returns no results' do
+        visit root_path
+        click_link 'Generate Realisations Report'
 
-      expect(page).to have_text 'Realisations Report Criteria'
-      click_button 'Submit' # submit with invalid form data
-      expect(page).to have_text "can't be blank"
+        expect(page).to have_text 'Realisations Report Criteria'
+        click_button 'Submit' # submit with invalid form data
+        expect(page).to have_text "can't be blank"
 
-      fill_in 'What is the start date for your report?', with: report_start_date
-      fill_in 'What is the end date for your report?', with: report_end_date
-      click_button 'Submit'
+        fill_in 'What is the start date for your report?', with: report_start_date
+        fill_in 'What is the end date for your report?', with: report_end_date
+        click_button 'Submit'
 
-      expect(page).to have_text 'Data extract found 0 rows'
-      expect(page).to have_text "Report Start Date #{report_start_date}"
-      expect(page).to have_text "Report End Date #{report_end_date}"
+        expect(page).to have_text 'Data extract found 0 rows'
+        expect(page).to have_text "Report Start Date #{report_start_date}"
+        expect(page).to have_text "Report End Date #{report_end_date}"
 
-      expect(page).not_to have_button('Download Report')
+        expect(page).not_to have_button('Download Report')
 
-      click_link 'Try again'
-      expect(page).to have_text 'Realisations Report Criteria'
+        click_link 'Try again'
+        expect(page).to have_text 'Realisations Report Criteria'
+      end
     end
   end
-
 end
