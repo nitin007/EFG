a35b38fa970e6faf69c47252879b7231b27ee219
Ben Pickles <spideryoung@gmail.com>
2015-04-08 18:24:19 +0100

    Cleanup.

--- spec/requests/reports/realisation_report_spec.rb
+++ spec/requests/reports/realisation_report_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe 'Realisation report' do
-  let!(:loan1) { FactoryGirl.create(:loan, :settled, settled_on: 10.days.ago.to_date )}
-  let!(:loan2) { FactoryGirl.create(:loan, :settled, settled_on: 11.days.ago.to_date )}
-  let!(:loan3) { FactoryGirl.create(:loan, :settled, settled_on: 12.days.ago.to_date )}
-  let!(:loan4) { FactoryGirl.create(:loan, :settled, settled_on: 13.days.ago.to_date )}
-  let!(:loan5) { FactoryGirl.create(:loan, :settled, settled_on: 14.days.ago.to_date )}
+  let(:loan1) { FactoryGirl.create(:loan, :settled, settled_on: 10.days.ago.to_date )}
+  let(:loan2) { FactoryGirl.create(:loan, :settled, settled_on: 11.days.ago.to_date )}
+  let(:loan3) { FactoryGirl.create(:loan, :settled, settled_on: 12.days.ago.to_date )}
+  let(:loan4) { FactoryGirl.create(:loan, :settled, settled_on: 13.days.ago.to_date )}
+  let(:loan5) { FactoryGirl.create(:loan, :settled, settled_on: 14.days.ago.to_date )}
 
   let!(:realisation1) { FactoryGirl.create(:loan_realisation, :pre,
                                                 realised_loan: loan1,
@@ -40,13 +40,6 @@ describe 'Realisation report' do
                                                 realised_amount: Money.new(4_000_00),
                                                 realised_on: 2.days.ago.to_date) }
 
-
-  let(:lender1) { realisation1.realised_loan.lender }
-  let(:lender2) { realisation2.realised_loan.lender }
-  let(:lender3) { realisation3.realised_loan.lender }
-  let(:lender4) { realisation4.realised_loan.lender }
-  let(:lender5) { realisation5.realised_loan.lender }
-
   let(:lender1) { loan1.lender }
   let(:lender2) { loan2.lender }
   let(:lender3) { loan3.lender }
@@ -60,8 +53,8 @@ describe 'Realisation report' do
     login_as(current_user, scope: :user)
   end
 
-  context 'as an CFE user' do
-    let!(:current_user) { FactoryGirl.create(:cfe_user) }
+  context 'as a CFE user' do
+    let(:current_user) { FactoryGirl.create(:cfe_user) }
 
     context 'when selecting particular lenders' do
       it 'outputs a CSV realisation report' do
@@ -74,10 +67,10 @@ describe 'Realisation report' do
 
         fill_in 'What is the start date for your report?', with: report_start_date
         fill_in 'What is the end date for your report?', with: report_end_date
-        select "#{lender1.name}", from: 'Which lenders would you like to see realisations for?'
-        select "#{lender2.name}", from: 'Which lenders would you like to see realisations for?'
-        select "#{lender3.name}", from: 'Which lenders would you like to see realisations for?'
-        select "#{lender4.name}", from: 'Which lenders would you like to see realisations for?'
+        select lender1.name, from: 'Which lenders would you like to see realisations for?'
+        select lender2.name, from: 'Which lenders would you like to see realisations for?'
+        select lender3.name, from: 'Which lenders would you like to see realisations for?'
+        select lender4.name, from: 'Which lenders would you like to see realisations for?'
         click_button 'Submit'
 
         expect(page).to have_text 'Data extract found 3 rows'
@@ -131,7 +124,7 @@ describe 'Realisation report' do
 
         fill_in 'What is the start date for your report?', with: report_start_date
         fill_in 'What is the end date for your report?', with: report_end_date
-        select "#{lender1.name}", from: 'Which lenders would you like to see realisations for?'
+        select lender1.name, from: 'Which lenders would you like to see realisations for?'
         click_button 'Submit'
 
         expect(page).to have_text 'Data extract found 0 rows'
