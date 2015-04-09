cd98ca324b94a8eb177e3e396a6ce6e94369a8b0
Neil Atkinson <neil@new-bamboo.co.uk>
2015-04-09 17:54:09 +0100

    Ensure dates display correctly by being explicit in test.

--- spec/requests/reports/recoveries_report_spec.rb
+++ spec/requests/reports/recoveries_report_spec.rb
@@ -1,29 +1,26 @@
 require 'spec_helper'
 
 describe 'Recoveries report' do
-  let!(:loan1) { FactoryGirl.create(:loan, :settled, settled_on: 10.days.ago.to_date )}
-  let!(:loan2) { FactoryGirl.create(:loan, :settled, settled_on: 11.days.ago.to_date )}
-  let!(:loan3) { FactoryGirl.create(:loan, :settled, settled_on: 12.days.ago.to_date )}
-  let!(:loan4) { FactoryGirl.create(:loan, :settled, settled_on: 13.days.ago.to_date )}
-  let!(:loan5) { FactoryGirl.create(:loan, :settled, settled_on: 14.days.ago.to_date )}
+  let!(:loan1) { FactoryGirl.create(:loan, :settled, settled_on: Date.new(2015, 3, 1) )}
+  let!(:loan2) { FactoryGirl.create(:loan, :settled, settled_on: Date.new(2015, 3, 2) )}
+  let!(:loan3) { FactoryGirl.create(:loan, :settled, settled_on: Date.new(2015, 3, 3) )}
+  let!(:loan4) { FactoryGirl.create(:loan, :settled, settled_on: Date.new(2015, 3, 4) )}
+  let!(:loan5) { FactoryGirl.create(:loan, :settled, settled_on: Date.new(2015, 3, 5) )}
 
-  let!(:recovery1) { FactoryGirl.create(:recovery, :realised, loan: loan1, recovered_on: 2.day.ago.to_date) }
-  let!(:recovery1b) { FactoryGirl.create(:recovery, :realised, loan: loan1, recovered_on: 3.day.ago.to_date) }
-  let!(:recovery1c) { FactoryGirl.create(:recovery, :realised, loan: loan1, recovered_on: Date.today) }
-  let!(:recovery1d) { FactoryGirl.create(:recovery, :realised, loan: loan1, recovered_on: 5.day.ago.to_date) }
-  let!(:recovery2) { FactoryGirl.create(:recovery, :realised, loan: loan2, recovered_on: 5.day.ago.to_date) }
-  let!(:recovery3) { FactoryGirl.create(:recovery, :unrealised, loan: loan3, recovered_on: Date.today) }
-  let!(:recovery4) { FactoryGirl.create(:recovery, :realised, loan: loan4, recovered_on: 2.day.ago.to_date) }
-  let!(:recovery5) { FactoryGirl.create(:recovery, :unrealised, loan: loan5, recovered_on: 2.day.ago.to_date) }
+  let!(:recovery1) { FactoryGirl.create(:recovery, :realised, loan: loan1, recovered_on: Date.new(2015, 4, 8)) }
+  let!(:recovery1b) { FactoryGirl.create(:recovery, :realised, loan: loan1, recovered_on: Date.new(2015, 4, 7)) }
+  let!(:recovery1c) { FactoryGirl.create(:recovery, :realised, loan: loan1, recovered_on: Date.new(2015, 4, 9)) }
+  let!(:recovery1d) { FactoryGirl.create(:recovery, :realised, loan: loan1, recovered_on: Date.new(2015, 4, 5)) }
+  let!(:recovery2) { FactoryGirl.create(:recovery, :realised, loan: loan2, recovered_on: Date.new(2015, 4, 5)) }
+  let!(:recovery3) { FactoryGirl.create(:recovery, :unrealised, loan: loan3, recovered_on: Date.new(2015, 4, 9)) }
+  let!(:recovery4) { FactoryGirl.create(:recovery, :realised, loan: loan4, recovered_on: Date.new(2015, 4, 8)) }
+  let!(:recovery5) { FactoryGirl.create(:recovery, :unrealised, loan: loan5, recovered_on: Date.new(2015, 4, 8)) }
 
   let(:lender1) { loan1.lender }
   let(:lender2) { loan2.lender }
   let(:lender3) { loan3.lender }
   let(:lender4) { loan4.lender }
   let(:lender5) { loan5.lender }
-
-  let(:report_start_date) { 4.days.ago.to_date.to_s }
-  let(:report_end_date) { 2.day.ago.to_date.to_s }
 
   before(:each) do
     login_as(current_user, scope: :user)
@@ -34,9 +31,6 @@ describe 'Recoveries report' do
 
     context 'when selecting particular lenders' do
       it 'outputs a CSV realisation report' do
-        report_start_date = 4.days.ago.to_date.to_s
-        report_end_date = 2.day.ago.to_date.to_s
-
         visit root_path
         click_link 'Generate Recoveries Report'
 
@@ -44,8 +38,8 @@ describe 'Recoveries report' do
         click_button 'Submit' # submit with invalid form data
         expect(page).to have_text "can't be blank"
 
-        fill_in 'What is the start date for your report?', with: report_start_date
-        fill_in 'What is the end date for your report?', with: report_end_date
+        fill_in 'What is the start date for your report?', with: "6/4/2015"
+        fill_in 'What is the end date for your report?', with: "8/4/2015"
         select "#{lender1.name}", from: 'Which lenders would you like to see recoveries for?'
         select "#{lender2.name}", from: 'Which lenders would you like to see recoveries for?'
         select "#{lender3.name}", from: 'Which lenders would you like to see recoveries for?'
@@ -53,8 +47,8 @@ describe 'Recoveries report' do
         click_button 'Submit'
 
         expect(page).to have_text 'Data extract found 3 rows'
-        expect(page).to have_text "Report Start Date #{report_start_date}"
-        expect(page).to have_text "Report End Date #{report_end_date}"
+        expect(page).to have_text "Report Start Date 06/04/2015"
+        expect(page).to have_text "Report End Date 08/04/2015"
         expect(page).to have_text "Lenders #{lender1.name}, #{lender2.name}, #{lender3.name}, #{lender4.name}"
         expect(page).not_to have_text lender5.name
         click_button 'Download Report'
@@ -65,8 +59,6 @@ describe 'Recoveries report' do
 
     context 'when selecting ALL lenders' do
       it 'outputs a CSV realisation report for all lenders' do
-        report_start_date = 4.days.ago.to_date.to_s
-        report_end_date = 2.day.ago.to_date.to_s
         all_lenders_list = Lender.all.map(&:name).join(', ')
 
         visit root_path
@@ -76,14 +68,14 @@ describe 'Recoveries report' do
         click_button 'Submit' # submit with invalid form data
         expect(page).to have_text "can't be blank"
 
-        fill_in 'What is the start date for your report?', with: report_start_date
-        fill_in 'What is the end date for your report?', with: report_end_date
+        fill_in 'What is the start date for your report?', with: "6/4/2015"
+        fill_in 'What is the end date for your report?', with: "8/4/2015"
         select "All", from: 'Which lenders would you like to see recoveries for?'
         click_button 'Submit'
 
         expect(page).to have_text 'Data extract found 4 rows'
-        expect(page).to have_text "Report Start Date #{report_start_date}"
-        expect(page).to have_text "Report End Date #{report_end_date}"
+        expect(page).to have_text "Report Start Date 06/04/2015"
+        expect(page).to have_text "Report End Date 08/04/2015"
         expect(page).to have_text "Lenders #{all_lenders_list}"
         click_button 'Download Report'
 
@@ -93,9 +85,6 @@ describe 'Recoveries report' do
 
     context 'when report criteria returns no results' do
       it 'a csv file cannot be downloaded' do
-        report_start_date = 10.days.ago.to_date.to_s
-        report_end_date = 9.days.ago.to_date.to_s
-
         visit root_path
         click_link 'Generate Recoveries Report'
 
@@ -103,14 +92,14 @@ describe 'Recoveries report' do
         click_button 'Submit' # submit with invalid form data
         expect(page).to have_text "can't be blank"
 
-        fill_in 'What is the start date for your report?', with: report_start_date
-        fill_in 'What is the end date for your report?', with: report_end_date
+        fill_in 'What is the start date for your report?', with: "3/1/2013"
+        fill_in 'What is the end date for your report?', with: "1/2/2013"
         select "#{lender1.name}", from: 'Which lenders would you like to see recoveries for?'
         click_button 'Submit'
 
         expect(page).to have_text 'Data extract found 0 rows'
-        expect(page).to have_text "Report Start Date #{report_start_date}"
-        expect(page).to have_text "Report End Date #{report_end_date}"
+        expect(page).to have_text "Report Start Date 03/01/2013"
+        expect(page).to have_text "Report End Date 01/02/2013"
         expect(page).to have_text "Lenders #{lender1.name}"
 
         expect(page).not_to have_button('Download Report')
@@ -130,8 +119,8 @@ describe 'Recoveries report' do
       click_link 'Generate Recoveries Report'
 
       expect(page).to have_text 'Recoveries Report Criteria'
-      fill_in 'What is the start date for your report?', with: 4.days.ago.to_date.to_s
-      fill_in 'What is the end date for your report?', with: 1.day.ago.to_date.to_s
+      fill_in 'What is the start date for your report?', with: '6/4/2015'
+      fill_in 'What is the end date for your report?', with: '8/4/2015'
       expect(page).not_to have_text('Which lenders would you like to see recoveries for?')
 
       click_button 'Submit'
@@ -144,9 +133,6 @@ describe 'Recoveries report' do
     end
 
     it 'a csv file cannot be downloaded when report criteria returns no results' do
-      report_start_date = 10.days.ago.to_date.to_s
-      report_end_date = 9.days.ago.to_date.to_s
-
       visit root_path
       click_link 'Generate Recoveries Report'
 
@@ -154,13 +140,13 @@ describe 'Recoveries report' do
       click_button 'Submit' # submit with invalid form data
       expect(page).to have_text "can't be blank"
 
-      fill_in 'What is the start date for your report?', with: report_start_date
-      fill_in 'What is the end date for your report?', with: report_end_date
+      fill_in 'What is the start date for your report?', with: "3/1/2013"
+      fill_in 'What is the end date for your report?', with: "1/2/2013"
       click_button 'Submit'
 
       expect(page).to have_text 'Data extract found 0 rows'
-      expect(page).to have_text "Report Start Date #{report_start_date}"
-      expect(page).to have_text "Report End Date #{report_end_date}"
+      expect(page).to have_text "Report Start Date 03/01/2013"
+      expect(page).to have_text "Report End Date 01/02/2013"
 
       expect(page).not_to have_button('Download Report')
 
